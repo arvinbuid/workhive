195ade137f3fa8e09f7f8fbbfbfb8b5c09d13fe0
Arvin Buid <arvinbuid01@gmail.com>
2025-01-19 18:26:40 +0800

    Added get routes.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs.';
+});
+
+/// Route with constraint
+Route::get('/jobs/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post ' . $id . ' Comment ' . $commentId;
+})->whereNumber('id')->whereNumber('commentId'); 
