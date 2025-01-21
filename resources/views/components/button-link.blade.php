bb9af50a1b72ab8e7a8564d76d74b7ad182689cb
Arvin Buid <arvinbuid01@gmail.com>
2025-01-21 19:05:21 +0800

    Made the nav link and button link reusable using mobile prop for nav link and block prop for button link component. Use nav link component inside header component.

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -3,11 +3,14 @@
     'bgClass' => 'bg-yellow-400',
     'hoverClass' => 'hover:bg-yellow-600',
     'textClass' => 'text-black',
+    'block' => false,
     'icon' => null,
 ])
 
 <a href={{ $url }}
-    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 {{ $bgClass }}">
+    class="{{ $bgClass }}
+        {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300
+        {{ $bgClass }} {{ $block ? 'block' : '' }}">
     @if ($icon)
         <i class="fa fa-{{ $icon }}"></i>
     @endif
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,19 @@
-@props(['url' => '/', 'active' => false, 'icon' => null])
+@props(['url' => '/', 'active' => false, 'icon' => null, 'mobile' => false])
 
-<a href={{ $url }} class="hover:underline py-2 {{ $active ? 'text-yellow-400 font-bold  ' : 'text-white' }}">
-    @if ($icon)
-        <i class="fa fa-{{ $icon }}"></i>
-    @endif
-    {{ $slot }}
-</a>
+@if ($mobile)
+    <a href={{ $url }}
+        class="block md:hidden text-white bg-blue-900 px-4 py-2 hover:bg-blue-700 pb-4 space-y-2 {{ $active ? 'text-yellow-400 font-bold  ' : '' }}">
+        @if ($icon)
+            <i class="fa fa-{{ $icon }} mr-1"></i>
+        @endif
+        {{ $slot }}
+    </a>
+@else
+    <a href={{ $url }}
+        class="hover:underline py-2 {{ $active ? 'text-yellow-400 font-bold  ' : 'text-white' }}">
+        @if ($icon)
+            <i class="fa fa-{{ $icon }} mr-1"></i>
+        @endif
+        {{ $slot }}
+    </a>
+@endif
