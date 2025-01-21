c80d69ca6ec3cad25d64d69c602adf0c8d8a7faf
Arvin Buid <arvinbuid01@gmail.com>
2025-01-21 20:42:40 +0800

    Created hero component.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,12 +8,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href={{ asset('css/style.css') }}>
     <title>{{ $title ?? 'Workhive | Find all jobs online' }}</title>
     @vite('resources/css/app.css')
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero title='Find your Dream Job' />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
