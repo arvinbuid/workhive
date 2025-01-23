e94fae99ae7d8872430bc5fccc604d9858961e78
Arvin Buid <arvinbuid01@gmail.com>
2025-01-23 19:40:57 +0800

    Improved create blade component and comment a code for development.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -39,12 +39,12 @@ class JobController extends Controller
             'description' => 'required|string'
         ]);
 
-        Job::create([
-            'title' => $validatedData['title'],
-            'description' => $validatedData['description'],
-        ]);
+        // Job::create([
+        //     'title' => $validatedData['title'],
+        //     'description' => $validatedData['description'],
+        // ]);
 
-        return redirect()->route('jobs.index');
+        // return redirect()->route('jobs.index');
     }
 
     /**
