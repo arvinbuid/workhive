127dfc23b103256ba2d7ad7c036b62b9457490c8
Arvin Buid <arvinbuid01@gmail.com>
2025-01-24 23:27:54 +0800

    Created alert component and use it inside main layout view component to display flash messages.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
     <x-slot name="title">Workhive | Jobs</x-slot>
-    <h1 class="text-2xl mb-4">Available Jobs</h1>
     <ul>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
             @forelse($jobs as $job)
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,10 +16,17 @@
 <body class="bg-gray-100">
     <x-header />
     @if (request()->is('/'))
-        <x-hero title='Find your Dream Job' />
+        <x-hero />
+        <x-top-banner />
     @endif
-    <x-top-banner />
     <main class="container mx-auto p-4 mt-4">
+        <!-- Display alert messages -->
+        @if (session('success'))
+            <x-alert type='success' message="{{ session('success') }}" />
+        @endif
+        @if (session('error'))
+            <x-alert type='error' message="{{ session('error') }}" />
+        @endif
         {{ $slot }}
     </main>
 
