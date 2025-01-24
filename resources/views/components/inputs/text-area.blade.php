91512b2e4d20acc374cd5000fd94f8268d41de75
Arvin Buid <arvinbuid01@gmail.com>
2025-01-24 11:21:59 +0800

    Created text-area, select and file components that are reusable

---
+++ resources/views/components/inputs/text-area.blade.php
@@ -0,0 +1,13 @@
+@props(['id', 'name', 'label' => null, 'value' => '', 'placeholder' => '', 'cols' => 30, 'rows' => 7])
+
+<div class="mb-4">
+    @if ($label)
+        <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>
+    @endif
+    <textarea id="{{ $id }}" name="{{ $name }}"
+        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
+        placeholder="{{ $placeholder }}" value="{{ old($name, $value) }}"></textarea>
+    @error($name)
+        <p class="text-red-500 mt-1">{{ $message }}</p>
+    @enderror
+</div>
