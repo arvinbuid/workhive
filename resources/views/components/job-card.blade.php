efb16c7765b7db5094f3d82f7a1a3a5b03017bb9
Arvin Buid <arvinbuid01@gmail.com>
2025-01-25 14:38:53 +0800

    Implemented hide or display job fields based on if it is required or not in show job listing and job card components, and update code in job controller.

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,7 +2,9 @@
 
 <div class="rounded-lg shadow-md bg-white p-4">
     <div class="flex items-center space-between gap-4">
-        <img src="/images/{{ $job->company_logo }}" alt={{ $job->company_name }} class="w-14" />
+        @if ($job->company_logo)
+            <img src="/images/{{ $job->company_logo }}" alt={{ $job->company_name }} class="w-14" />
+        @endif
         <div>
             <h2 class="text-xl font-semibold">
                 {{ $job->title }}
@@ -15,6 +17,7 @@
     </p>
     <ul class="my-4 bg-gray-100 p-4 rounded">
         <li class="mb-2"><strong>Salary:</strong> ${{ number_format($job->salary) }}</li>
+
         <li class="mb-2">
             <strong>Location:</strong> {{ $job->city }}, {{ $job->state }}
             @if ($job->remote)
@@ -23,9 +26,11 @@
                 <span class="text-xs bg-red-500 text-white rounded-full px-2 py-1 ml-2">On-site</span>
             @endif
         </li>
-        <li class="mb-2">
-            <strong>Tags:</strong> <span>{{ ucwords(str_replace(',', ', ', $job->tags)) }}</span>
-        </li>
+        @if ($job->tags)
+            <li class="mb-2">
+                <strong>Tags:</strong> <span>{{ ucwords(str_replace(',', ', ', $job->tags)) }}</span>
+            </li>
+        @endif
     </ul>
     <a href={{ route('jobs.show', $job->id) }}
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
