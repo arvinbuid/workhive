231751f1dd8cfe530c5d79330b5a3f2b8c21c6ca
Arvin Buid <arvinbuid01@gmail.com>
2025-01-30 22:31:09 +0800

    Created search component and use it inside hero and jobs/index components/

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     <x-slot name="title">Workhive | Jobs</x-slot>
+    <div class="bg-blue-900 p-6 flex justify-center items-center mb-4 w-full">
+        <x-search />
+    </div>
     <ul>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
             @forelse($jobs as $job)
